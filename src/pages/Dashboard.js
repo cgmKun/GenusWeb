c20df25a1d09679b841bd8dc5333bfeb69c081b2
Aylin <A01379272@itesm.mx>
2022-05-01 22:17:20 -0500

    Graph

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,21 +1,23 @@
 import { React, Component } from "react";
+import Container from 'react-bootstrap/Container'
 
 //import DeffectModal from "../components/Dashboard/DeffectModal";
 import DefectsOnGroup from "../components/Dashboard/DefectsOnGroup";
 import GroupCards from "../components/Dashboard/GroupCards";
-//import DeffectTable from "../components/Dashboard/DeffectTable";
-//import { Empty } from '@douyinfe/semi-ui';
-//import underConstruction from '../images/istockphoto-1273109788-612x612.jpeg';
+import GraphGroup from "../components/Dashboard/GraphGroup";
 
 import "../styles/Dashboard.scss"
 
 class Dashboard extends Component {
     render () {
         return(
-            <div className="container">
-                <GroupCards/>
+            <Container fluid className="container">
+                <div className="chart-area" >
+                    <GraphGroup/>
+                    <GroupCards/>
+                </div>
                 <DefectsOnGroup/>
-            </div>
+            </Container>
         )
     }
 }
